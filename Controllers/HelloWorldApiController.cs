f5de0e4341ccc694cf9f54fb69e0f4b65402fafa
Nader <naderzenedi@gmail.com>
2024-08-20 21:28:19 -0300

    Commit do Projeto

---
+++ Controllers/HelloWorldApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace HelloWorldWebApp.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class HelloWorldApiController : ControllerBase
+{
+    // GET: api/helloworldapi
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        Random rand = new Random();
+        double temperatura = rand.NextDouble() * 50;
+
+        if (temperatura >= 35)
+        {
+            string mensagem = $"A temperatura atingiu o valor de {temperatura:F2} graus";
+            Console.WriteLine($"[x] Gerado: {mensagem}");
+            return Ok(mensagem);
+        }
+
+        return Ok($"Temperatura abaixo do limite. A temperatura atingiu o valor de {temperatura:F2} graus");
+    }
+}
